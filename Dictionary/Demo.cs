7e3ef08df5346b43cf641669c78843016e071808
LAPTOP-EFSKR9KV\ramya <ramya0502@gmail.com>
2022-11-11 20:21:05 +0000

    dt11/12/2021

---
+++ Dictionary/Demo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    public class Demo
+    {
+        public void x1()
+
+        {
+            Dictionary<string, object> s = new Dictionary<string, object>();
+
+            s.Add("mahesh", 554433d);
+
+            s.Add("sureah", "ramya");
+
+            foreach (string m in s.Keys) 
+
+            Console.WriteLine(m + "=="+s[m]);
+
+
+
+
+
+
+
+
+
+        }
+
+    }
+
+}
+
+
---
+++ Dictionary/Sample.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    internal class Sample
+    {
+        static void Main(string[] args)
+        {
+            Demo q = new Demo();
+
+            q.x1();
+
+
+
+
+        }
+
+
+
+
+
+    }
+}
