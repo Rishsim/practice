a0e0b99b2aa02401798b16a2a50e0c6d971869a4
LAPTOP-EFSKR9KV\ramya <ramya0502@gmail.com>
2022-11-11 20:11:46 +0000

    11/12/2021

---
+++ AL/Demo.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AL
+{
+    public class Demo
+    {
+
+        public void S1()
+        {
+            ArrayList a = new ArrayList();
+
+            a.Add(10);
+
+            a.Add("rish");
+
+            Console.WriteLine(a[0]);
+
+            a.Add(10.5f);
+
+            a.Add(223344d);
+
+            a.Add('s');
+            
+            a.Add(true);
+
+            foreach(var b in a)
+            {
+                Console.WriteLine(b);
+
+
+
+            }
+        
+        
+        
+        
+        }
+       
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ AL/Sample.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AL
+{
+    internal class Sample
+    {
+
+
+        static void Main(string[] args)
+        {
+
+
+
+            Demo c = new Demo();
+
+            c.S1();
+        }
+    }
+}
